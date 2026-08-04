43b291bc5bb6bedab74f028d4fa2f37a9264c2d8
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-04 15:36:13 -0600

    9.5 WebBMI -Completado

--- ts-exercises/bmiCalculator.ts
+++ ts-exercises/bmiCalculator.ts
@@ -1,20 +1,9 @@
 export const calculateBmi = (height: number, weight: number): string => {
-  const heightInMeters = height / 100;
-  const bmi = weight / (heightInMeters * heightInMeters);
+  const heightInMeters = height / 100
+  const bmi = weight / (heightInMeters * heightInMeters)
 
-  if (bmi < 18.5) return "Underweight";
-  if (bmi < 25) return "Normal (healthy weight)";
-  if (bmi < 30) return "Overweight";
-  return "Obese";
-};
-
-const height = Number(process.argv[2]);
-const weight = Number(process.argv[3]);
-
-if (!height || !weight || isNaN(height) || isNaN(weight)) {
-  console.log(
-    "Please provide height and weight as numbers: npm run calculateBmi <height> <weight>",
-  );
-} else {
-  console.log(calculateBmi(height, weight));
+  if (bmi < 18.5) return 'Underweight'
+  if (bmi < 25) return 'Normal (healthy weight)'
+  if (bmi < 30) return 'Overweight'
+  return 'Obese'
 }
--- ts-exercises/index.ts
+++ ts-exercises/index.ts
@@ -1,13 +1,35 @@
-import express from 'express'
+import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 
-const app = express()
+const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
 
-const PORT = 3000
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (
+    !req.query.height ||
+    !req.query.weight ||
+    isNaN(height) ||
+    isNaN(weight)
+  ) {
+    res.status(400).json({ error: "malformatted parameters" });
+    return;
+  }
+
+  res.json({
+    weight,
+    height,
+    bmi: calculateBmi(height, weight),
+  });
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
