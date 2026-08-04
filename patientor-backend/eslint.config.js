e88ad689ef72d8da1678eadb2c60333e0de2d8fb
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-04 16:17:24 -0600

    9.8: Patientor backend, paso 1  -Completado

---
+++ patientor-backend/eslint.config.js
@@ -0,0 +1,21 @@
+const typescriptEslint = require('@typescript-eslint/eslint-plugin')
+const typescriptParser = require('@typescript-eslint/parser')
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
+  }
+]
