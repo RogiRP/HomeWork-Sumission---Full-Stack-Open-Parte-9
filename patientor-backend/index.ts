8c4f92ead43046733af67585d250dfa1a1f533a1
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-04 16:26:31 -0600

    9.9: Patientor backend, paso 2 -Completado

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,14 +1,16 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/api/ping', (_req, res) => {
-  res.send('pong')
-})
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
 
-const PORT = 3001
+const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
