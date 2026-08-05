41a05178cf21654724809b67c535902cadc50edd
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-05 16:19:14 -0600

    9.24: Patientor, paso 5  -Completado

--- patientor/src/components/PatientPage/index.tsx
+++ patientor/src/components/PatientPage/index.tsx
@@ -1,45 +1,65 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { Typography, Box } from "@mui/material"
-import { Male, Female, Transgender } from "@mui/icons-material"
-import { Patient, Gender, Entry } from "../../types"
-import patientService from "../../services/patients"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+import { Male, Female, Transgender } from "@mui/icons-material";
+import { Patient, Gender, Entry, Diagnosis } from "../../types";
+import patientService from "../../services/patients";
 
 const GenderIcon = ({ gender }: { gender: Gender }) => {
   switch (gender) {
-    case Gender.Male: return <Male />
-    case Gender.Female: return <Female />
-    default: return <Transgender />
+    case Gender.Male:
+      return <Male />;
+    case Gender.Female:
+      return <Female />;
+    default:
+      return <Transgender />;
   }
-}
+};
 
-const EntryDetails = ({ entry }: { entry: Entry }) => {
+const EntryDetails = ({
+  entry,
+  diagnoses,
+}: {
+  entry: Entry;
+  diagnoses: Diagnosis[];
+}) => {
   return (
     <Box sx={{ border: 1, borderRadius: 1, padding: 1, marginBottom: 1 }}>
-      <Typography>{entry.date} <em>{entry.type}</em></Typography>
+      <Typography>
+        {entry.date} <em>{entry.type}</em>
+      </Typography>
       <Typography>{entry.description}</Typography>
       {entry.diagnosisCodes && (
         <ul>
-          {entry.diagnosisCodes.map(code => (
-            <li key={code}>{code}</li>
-          ))}
+          {entry.diagnosisCodes.map((code) => {
+            const diagnosis = diagnoses.find((d) => d.code === code);
+            return (
+              <li key={code}>
+                {code} {diagnosis?.name}
+              </li>
+            );
+          })}
         </ul>
       )}
     </Box>
-  )
+  );
+};
+
+interface Props {
+  diagnoses: Diagnosis[];
 }
 
-const PatientPage = () => {
-  const { id } = useParams<{ id: string }>()
-  const [patient, setPatient] = useState<Patient | null>(null)
+const PatientPage = ({ diagnoses }: Props) => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
 
   useEffect(() => {
     if (id) {
-      patientService.getById(id).then(setPatient)
+      patientService.getById(id).then(setPatient);
     }
-  }, [id])
+  }, [id]);
 
-  if (!patient) return <div>loading...</div>
+  if (!patient) return <div>loading...</div>;
 
   return (
     <Box>
@@ -48,12 +68,14 @@ const PatientPage = () => {
       </Typography>
       <Typography>ssn: {patient.ssn}</Typography>
       <Typography>occupation: {patient.occupation}</Typography>
-      <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 1 }}>entries</Typography>
-      {patient.entries.map(entry => (
-        <EntryDetails key={entry.id} entry={entry} />
+      <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 1 }}>
+        entries
+      </Typography>
+      {patient.entries.map((entry) => (
+        <EntryDetails key={entry.id} entry={entry} diagnoses={diagnoses} />
       ))}
     </Box>
-  )
-}
+  );
+};
 
-export default PatientPage
+export default PatientPage;
--- patientor/src/services/patients.ts
+++ patientor/src/services/patients.ts
@@ -1,20 +1,25 @@
-import axios from "axios";
-import { Patient, PatientFormValues } from "../types";
-import { apiBaseUrl } from "../constants";
+import axios from "axios"
+import { Patient, PatientFormValues, Diagnosis } from "../types"
+import { apiBaseUrl } from "../constants"
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`);
-  return data;
-};
+  const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`)
+  return data
+}
 
 const getById = async (id: string) => {
-  const { data } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
-  return data;
-};
+  const { data } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`)
+  return data
+}
+
+const getDiagnoses = async () => {
+  const { data } = await axios.get<Diagnosis[]>(`${apiBaseUrl}/diagnoses`)
+  return data
+}
 
 const create = async (object: PatientFormValues) => {
-  const { data } = await axios.post<Patient>(`${apiBaseUrl}/patients`, object);
-  return data;
-};
+  const { data } = await axios.post<Patient>(`${apiBaseUrl}/patients`, object)
+  return data
+}
 
-export default { getAll, getById, create };
+export default { getAll, getById, getDiagnoses, create }
